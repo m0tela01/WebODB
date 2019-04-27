65adf2572ce53240bf1abd2a05db6294a41e7987
m0tela01 <michael.telahun1@gmail.com>
2019-04-27 15:59:10 -0400

    Add new gauges and data from latest car data. Remove old stuff. Test connection for obd reader. Get ready for final steps.

--- WebODBApp/src/app/odb/odb.component.ts
+++ WebODBApp/src/app/odb/odb.component.ts
@@ -46,6 +46,10 @@ export class OdbComponent implements OnInit {
 	gaugeValueLoad: any;
 	gaugeValueRPM: any;
 	gaugeValueThrottle: any;
+	gaugeValueSpeedInKPH: any;
+	gaugeValueSpeedInMPH: any;
+	gaugeValueAirIntake: any;
+	gaugeValueDegreeRaceCar: any;
 	
 	gaugeColor= "rgba(0, 221, 166)"
 	obd2: any;
@@ -57,8 +61,7 @@ export class OdbComponent implements OnInit {
 	public innerWidth: any;
 	ngOnInit() {
 
-		var dog = [];
-		var fromDataBase = this.db.list('/')
+		this.db.list('/')
 		.snapshotChanges().subscribe(
 			item => {
 				setTimeout(() => {
@@ -69,140 +72,20 @@ export class OdbComponent implements OnInit {
 					this.gaugeValueLoad = this.obd2[0]["EngineLoad"];
 					this.gaugeValueRPM = this.obd2[0]["RPM"]
 					this.gaugeValueThrottle = this.obd2[0]["ThrottlePosition"]
-				}, 100);
-
+					this.gaugeValueSpeedInKPH = this.obd2[0]["SpeedKPH"]
+					this.gaugeValueSpeedInMPH = this.obd2[0]["SpeedMPH"]
+					this.gaugeValueAirIntake = this.obd2[0]["AirIntake"]
+					this.gaugeValueDegreeRaceCar = this.obd2[0]["Degree"]
+				}, 1000);
 			}
-			
 		)
-		
-
-		this.innerWidth = window.innerWidth;
-
-		
-
-
-		let chart = new CanvasJS.Chart("chartContainer", {
-			animationEnabled: true,
-			exportEnabled: true,
-			creditText: "",
-			backgroundColor: "#00ffffff",
-			title: {
-				text: "Basic Column Chart in Angular"
-			},
-			data: [{
-				type: "column",
-				dataPoints: [
-					{ y: 71, label: "Apple" },
-					{ y: 55, label: "Mango" },
-					{ y: 50, label: "Orange" },
-					{ y: 65, label: "Banana" },
-					{ y: 95, label: "Pineapple" },
-					{ y: 68, label: "Pears" },
-					{ y: 28, label: "Grapes" },
-					{ y: 34, label: "Lychee" },
-					{ y: 14, label: "Jackfruit" }
-				]
-			}]
-		});
-		chart.render();
-
-		let dataPoints = [];
-		let y = 0;		
-		for ( var i = 0; i < 10000; i++ ) {		  
-			y += Math.round(5 + Math.random() * (-5 - 5));	
-			dataPoints.push({ y: y});
-		}
-
-		let chart2 = new CanvasJS.Chart("chartContainer2", {
-			zoomEnabled: true,
-			animationEnabled: true,
-			exportEnabled: true,
-			title: {
-				text: "Performance Demo - 10000 DataPoints"
-			},
-			subtitles:[{
-				text: "Try Zooming and Panning"
-			}],
-			data: [
-			{
-				type: "line",                
-				dataPoints: dataPoints
-			}]
-		});
-			
-		chart2.render();
-		
-		let chart3 = new CanvasJS.Chart("chartContainer3", {
-			theme: "light2",
-			animationEnabled: true,
-			exportEnabled: true,
-			title:{
-				text: "Monthly Expense"
-			},
-			data: [{
-				type: "pie",
-				showInLegend: true,
-				toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
-				indexLabel: "{name} - #percent%",
-				dataPoints: [
-					{ y: 450, name: "Food" },
-					{ y: 120, name: "Insurance" },
-					{ y: 300, name: "Traveling" },
-					{ y: 800, name: "Housing" },
-					{ y: 150, name: "Education" },
-					{ y: 150, name: "Shopping"},
-					{ y: 250, name: "Others" }
-				]
-			}]
-		});
-			
-		chart3.render();
-
-
-		let dataPoints2 = [];
-		let dpsLength = 0;
-		let chart4 = new CanvasJS.Chart("chartContainer4",{
-			exportEnabled: true,
-			title:{
-				text:"Live Chart with Data-Points from External JSON"
-			},
-			data: [{
-				type: "spline",
-				dataPoints : dataPoints2,
-			}]
-		});
-		
-		$.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=25&length=20&type=json&callback=?", function(data) {  
-			$.each(data, function(key, value){
-				dataPoints2.push({x: value[0], y: parseInt(value[1])});
-			});
-			dpsLength = dataPoints2.length;
-			chart4.render();
-			updateChart();
-		});
-		function updateChart() {	
-			$.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dpsLength + 1) + "&ystart=" + (dataPoints2[dataPoints.length - 1].y) + "&length=1&type=json&callback=?", function(data) {
-				$.each(data, function(key, value) {
-					dataPoints2.push({
-					x: parseInt(value[0]),
-					y: parseInt(value[1])
-					});
-					dpsLength++;
-				});
-				
-				if (dataPoints2.length >  20 ) {
-							dataPoints2.shift();				
-						}
-				chart4.render();
-				setTimeout(function(){updateChart()}, 1000);
-			});
-		}
 	}
 
+	//row 1
 	coolant: OdbGauge = {
 		gaugeType: "semi",
 		gaugeLabel: "Coolant Temperature",
-		gaugeAppendText: "°C",
+		gaugeAppendText: " °C",
 		gaugeColor2: "rgba(236, 29, 14)",
 		gaugeColor: "rgba(0, 221, 166)",
 		gaugeAnimate: "true",
@@ -216,7 +99,7 @@ export class OdbComponent implements OnInit {
 	};
 
 	load: OdbGauge = {
-		gaugeType: "semi",
+		gaugeType: "full",
 		gaugeLabel: "Engine Load",
 		gaugeAppendText: " %",
 		gaugeColor2: "rgba(236, 29, 14)",
@@ -232,7 +115,7 @@ export class OdbComponent implements OnInit {
 	}; 
 
 	rpm: OdbGauge = {
-		gaugeType: "semi",
+		gaugeType: "full",
 		gaugeLabel: "RPM",
 		gaugeAppendText: "",
 		gaugeColor2: "rgba(236, 29, 14)",
@@ -248,7 +131,7 @@ export class OdbComponent implements OnInit {
 	};
 	
 	throttle: OdbGauge = {
-		gaugeType: "semi",
+		gaugeType: "full",
 		gaugeLabel: "Throttle Position",
 		gaugeAppendText: " %",
 		gaugeColor2: "rgba(236, 29, 14)",
@@ -260,6 +143,71 @@ export class OdbComponent implements OnInit {
 		gaugeDuration: "100",
 		gaugeSize: "375",
 		gaugeMax: "100",
+		gaugePrepend: ""
+	};
+		
+	// row 2
+	spdKph: OdbGauge = {
+		gaugeType: "semi",
+		gaugeLabel: "Speed",
+		gaugeAppendText: " KPH",
+		gaugeColor2: "rgba(236, 29, 14)",
+		gaugeColor: "rgba(61, 140, 241)",
+		gaugeAnimate: "false",
+		gaugeBackColor: "rgba(128, 128, 150)",
+		gaugeCap: "round",
+		gaugeThickness: "15",
+		gaugeDuration: "100",
+		gaugeSize: "375",
+		gaugeMax: "100",
+		gaugePrepend: ""
+	};
+
+	spdMph: OdbGauge = {
+		gaugeType: "semi",
+		gaugeLabel: "Speed",
+		gaugeAppendText: " MPH",
+		gaugeColor2: "rgba(236, 29, 14)",
+		gaugeColor: "rgba(247, 80, 164)",
+		gaugeAnimate: "false",
+		gaugeBackColor: "rgba(128, 128, 150)",
+		gaugeCap: "round",
+		gaugeThickness: "15",
+		gaugeDuration: "100",
+		gaugeSize: "375",
+		gaugeMax: "100",
+		gaugePrepend: ""
+	};
+
+	airIntake: OdbGauge = {
+		gaugeType: "semi",
+		gaugeLabel: "Air Intake",
+		gaugeAppendText: " °F",
+		gaugeColor2: "rgba(236, 29, 14)",
+		gaugeColor: "rgba(234, 235, 236)",
+		gaugeAnimate: "false",
+		gaugeBackColor: "rgba(128, 128, 150)",
+		gaugeCap: "round",
+		gaugeThickness: "15",
+		gaugeDuration: "100",
+		gaugeSize: "375",
+		gaugeMax: "100",
+		gaugePrepend: ""
+	};
+
+	raceCar: OdbGauge = {
+		gaugeType: "full",
+		gaugeLabel: "Degrees Racecar",
+		gaugeAppendText: " °",
+		gaugeColor2: "rgba(236, 29, 14)",
+		gaugeColor: "rgba(177, 93, 232)",
+		gaugeAnimate: "false",
+		gaugeBackColor: "rgba(128, 128, 150)",
+		gaugeCap: "round",
+		gaugeThickness: "15",
+		gaugeDuration: "100",
+		gaugeSize: "375",
+		gaugeMax: "5",
 		gaugePrepend: ""
 	};
 }
